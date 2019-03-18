1ea7ba5f2dd27a8f319646e492e9e86d509bb4cf
cserteGT3 <csertegt@gmail.com>
2019-03-18 16:59:02 +0100

    move example octree to module finalize octree example visualization

--- Diplomaterv/octree.jl
+++ Diplomaterv/octree.jl
@@ -53,4 +53,38 @@ function iswithinrectangle(rect::HyperRectangle, p)
     return true
 end
 
+end #module
+
+module ExamplePointCloud
+# construct an example pointcloud
+
+using StaticArrays: SVector
+export buildexamplecloud
+
+const maxV = [5.5, 10, 7.8];
+
+"""
+    buildexamplecloud(maxp = [5.5, 10, 7.8])
+
+Create an example point cloud for octree tests and visualization.
+
+One cornerpoint of the prism is the origin and the other can be given as `maxp`.
+Return tuple's fields: `corners`, `pc`, `minP`, `maxP`.
+"""
+function buildexamplecloud(maxp = [5.5, 10, 7.8])
+    @assert length(maxp) == 3
+    minp = [0.0, 0, 0];
+    mmV = [minp maxp];
+
+    # corner points
+    corners=[SVector(mmV[1,i], mmV[2,j], mmV[3,k]) for i in 1:2 for j in 1:2 for k in 1:2]
+
+    full8p=[SVector(0.75*maxp[1]+(i/12)*maxp[1], 0.75*maxp[2]+(j/12)*maxp[2], 0.75*maxp[3]+(k/12)*maxp[3]) for i in 0:2 for j in 0:2 for k in 0:2]
+    quadrant=[SVector((i/3)*maxp[1], (j/3)*maxp[2], (k/3)*maxp[3]) for i in 0:1 for j in 0:1 for k in 0:1]
+    online=[SVector((3/4)*maxp[1], (j/2)*maxp[2], (k/2)*maxp[3]) for j in 0:0.5:1 for k in 0:0.5:1]
+
+    pc = vcat(full8p, quadrant, online)
+    return (corners = corners, pc = pc, minP = minp, maxP = maxp)
 end
+
+end #module
--- Diplomaterv/test/visualizeoctree.jl
+++ Diplomaterv/test/visualizeoctree.jl
@@ -1,39 +1,29 @@
-## Octree test
+## Octree visualization
 
-# construct a pointcloud
+using Pkg
+pkg"activate"
+include("../octree.jl")
 
-using StaticArrays: SVector
+using .ExamplePointCloud
 
-minV = [0.0, 0, 0];
-maxV = [5.5, 10, 7.8];
-mmV = [minV maxV];
-
-# corner points
-corners=[SVector(mmV[1,i], mmV[2,j], mmV[3,k]) for i in 1:2 for j in 1:2 for k in 1:2]
-
-full8p=[SVector(0.75*maxV[1]+(i/12)*maxV[1], 0.75*maxV[2]+(j/12)*maxV[2], 0.75*maxV[3]+(k/12)*maxV[3]) for i in 0:2 for j in 0:2 for k in 0:2 ];
-quadrant=[SVector((i/3)*maxV[1], (j/3)*maxV[2], (k/3)*maxV[3]) for i in 0:1 for j in 0:1 for k in 0:1 ];
-online = [SVector((3/4)*maxV[1], (j/2)*maxV[2], (k/2)*maxV[3]) for j in 0:0.5:1 for k in 0:0.5:1 ];
-
-pc = vcat(full8p, quadrant, online)
-
+pcd = buildexamplecloud();
+corners = pcd.corners
+cloud = pcd.pc
 
 ## visualize
 
 using Makie
 s = Scene()
-scatter!(corners, color=:blue)
-scatter!(full8p, color=:red)
-scatter!(quadrant, color= :green)
-scatter!(online, color=:brown)
+scatter!(s, corners, color=:blue)
+scatter!(s, cloud, color= :green)
 
 # build octree
 using RegionTrees: adaptivesampling!, Cell, allleaves
 
-include("../octree.jl")
 using .Octree
-
-root = Cell(SVector{3}(minV), SVector{3}(maxV), OctreeNode(pc, collect(1:length(pc)), 0.0, 1))
+using StaticArrays: SVector
+import GeometryTypes
+root = Cell(SVector{3}(pcd.minP), SVector{3}(pcd.maxP), OctreeNode(cloud, collect(1:length(cloud)), 0.0, 1))
 
 r = OctreeRefinery(8)
 
@@ -57,8 +47,3 @@ for leaf in allleaves(root)
     global i +=1
 end
 s
-using GeometryTypes
-kubi = GeometryTypes.HyperRectangle(Vec3(root.boundary.origin...), Vec3(root.boundary.widths...))
-
-s22  = mesh(kubi, color = (:green, 0.2), transparency = true)
-text!(s22, "1", position = Vec3(root.boundary.origin...), textsize = 0.5)
