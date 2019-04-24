8bec087b2a3bf2fe58275ba38a0e65a88320b800
cserteGT3 <csertegt@gmail.com>
2019-04-24 21:08:09 +0200

    remove unnecessary lines and add working prototype of robotmodel

--- MRD/makie_robvis.jl
+++ MRD/makie_robvis.jl
@@ -40,18 +40,6 @@ function tori(R, r, lR, lr)
     ps = [SVector(x(i,j), y(i,j), z(i,j)) for i in range(0, 2π, length=lR) for j in range(0, 2π, length=lr)]
 end
 
-function array2arrays(A)
-    xout = Array{eltype(A[1])}(undef, length(A))
-    yout = Array{eltype(A[1])}(undef, length(A))
-    zout = Array{eltype(A[1])}(undef, length(A))
-    for i in eachindex(A)
-        xout[i] = A[i][1]
-        yout[i] = A[i][2]
-        zout[i] = A[i][3]
-    end
-    xout, yout, zout
-end
-
 o = SVector(0.0,0,0);
 ax = SVector(1,0,0.0);
 ay = SVector(0,1,0.0);
@@ -65,13 +53,13 @@ s = Scene();
 scatter!(s, sri)
 drawcf(s, o, ax, ay, az, lwidth = 5)
 
-function makietori(R, r, lR, lr)
+function makietori(R, r, lR, lr; z0 = 0)
     @assert R > r "Ring tori, I told you."
     theta = range(0, 2π, length=lR)
     phi = range(0, 2π, length=lr)
     x_(Θ, ϕ) = (R + r*cos(Θ))*cos(ϕ)
     y_(Θ, ϕ) = (R + r*cos(Θ))*sin(ϕ)
-    z_(Θ, ϕ) = r*sin(Θ)
+    z_(Θ, ϕ) = z0 + r*sin(Θ)
     x = [x_(θ, ϕ) for θ in theta, ϕ in phi]
     y = [y_(θ, ϕ) for θ in theta, ϕ in phi]
     z = [z_(θ, ϕ) for θ in theta, ϕ in phi]
@@ -80,3 +68,45 @@ end
 
 mx, my, mz = makietori(10,2, 500, 500);
 surface(mx, my, mz, colormap=[:green], transparency = false, shading = true)
+
+using StaticArrays
+using Makie
+using Colors
+
+## MRD homework parameters
+l10 = 0.8; #m
+l11 = 0.6; #m
+l20 = 0.3; #m
+
+o = SVector(0,0,0.0);
+v1 = SVector(0,0,l10);
+v11 = SVector(0,l11,0);
+v20 = SVector(0,0,l20);
+
+s1 = o+v1;
+s2 = s1+v11;
+s3 = s2+v20;
+
+segments = [o => s1; s1 => s2; s2 => s3];
+
+n = 100;
+mx, my, mz = makietori(l11, l20, n, n, z0 = l10);
+s = surface(mx, my, mz, color = fill(RGBA(1.,1.,1.,0.4), n, n), transparency = true, shading = true)
+linesegments!(s, segments, linewidth = 5)
+axis = s[Axis];
+axis[:showgrid] = false
+
+ax = SVector(1,0,0.0);
+ay = SVector(0,1,0.0);
+az = SVector(0,0,1.0);
+
+# x0: o, ax,ay,az
+# x1: s1, ay,-ax,az
+# x2: s2, ay,az,ax
+# x3: s3, ax, ay, az
+ll = 0.15;
+lw = 4;
+drawcf(s, o, ax, ay, az, llength = ll, lwidth = lw)
+drawcf(s, s1, ay, -ax, az, llength = ll, lwidth = lw)
+drawcf(s, s2, ay, az, ax, llength = ll, lwidth = lw)
+drawcf(s, s3, ax, ay, az, llength = ll, lwidth = lw)
