2ea81635d9c92806cdd0671d7a3ed8ef1b635e8b
cserteGT3 <csertegt@gmail.com>
2019-04-24 22:52:41 +0200

    dating with Images

--- Diplomaterv/skecher.jl
+++ Diplomaterv/skecher.jl
@@ -31,3 +31,35 @@ end
 #sc2 = scatter(parSp)
 
 hbox(scatter(tsP, markersize=2), scatter(parSp))
+
+## Dating with Images
+using Images
+using ImageView
+
+## basic test image
+using TestImages
+img = testimage("mandrill");
+imshow(img);
+
+## finding connected components on a binary image
+# https://juliaimages.org/latest/function_reference/#Images.label_components
+indexes = rand(Int,5,5);
+tk = rand(Bool,5,5);
+imshow(tk)
+
+# labeling
+lbs = label_components(tk);
+# 8 connectivity
+lbs8 = label_components(tk, trues(3,3))
+# size of the components
+lls = component_lengths(lbs)
+# largest connected component (first is the background)
+maxi = argmax(lls[2:end])+1
+idxs = findall(x->x==maxi-1, lbs)
+# get all the labels
+lbs[idxs]
+# get all the pixels
+tk[idxs]
+# get all the indexes
+indexes[idxs]
+# I don't know what is this for: idxs = component_indices(lbs)
