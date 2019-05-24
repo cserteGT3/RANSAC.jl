93cda2b6a27f52f63231a41087eca46d092dfbce
cserteGT3 <csertegt@gmail.com>
2019-05-24 20:04:57 +0200

    add and update visualizations for presentation

--- Diplomaterv/noisika.jl
+++ Diplomaterv/noisika.jl
@@ -1,8 +1,3 @@
-## RANSAC
-using Pkg
-Pkg.activate()
-
-# every include
 using LinearAlgebra
 using StaticArrays
 using RegionTrees
@@ -30,11 +25,11 @@ include("ransac.jl")
 
 ## Test1
 # inputs
-vs, ns, norms4Plot, shape_s = examplepc3();
+vs, ns, norms4Plot, shape_s = examplepc3(true, all = true, mrotdeg = 0.1, vertscale = 0.3);
 sc = showgeometry(vs, ns)
-sc.center = false
-#Makie.save("pure3-normals.png", sc)
-# (normalize surface normals if needed)
+# sc.center = false
+# Makie.save("pure3-normals.png", sc)
+
 pcr = PointCloud(vs, ns, 32);
 # plane
 p_ae = (ϵ = 0.3, α=deg2rad(5));
@@ -46,7 +41,7 @@ one_ae = AlphSilon(sp_ae, p_ae, cy_ae);
 # number of minimal subsets drawed in one iteration
 tt = 15;
 # probability that we found shapes
-ptt = 0.9
+ptt = 0.8
 # minimum shape size
 ττ = 900
 # maximum number of iteration
@@ -58,49 +53,41 @@ cand, extr = ransac(pcr, one_ae, tt, ptt, ττ, itermax, draws, 500, true)
 #leftover = getrest(pcr);
 showtype(extr)
 sc = showshapes(pcr, extr)
-
 showbytype(pcr, extr)
 
-m2 = vbox(sc, sc2)
+## TEST 2
 
-sco = scatter(vs)
-scon = showgeometry(vs, ns)
-m = vbox(scon, sc)
-# Makie.save("plot.png", m)
+# inputs
+vs, ns, norms4Plot, shape_s = examplepc3(true, all = true, mrotdeg = .7, vertscale = 1.1);
 
-scatter(pcr.vertices[leftover])
-linesegments!(norms4Plot[leftover], color = :blue)
+sc = showgeometry(vs, ns)
+#sc.center = false
+#Makie.save("sc.png", sc)
 
-showgeometry(vs, ns)
-## Test2
-# noised
-vs2, ns2, norms4Plot2, shape_s2 = examplepc3(true, all = true, mrotdeg = 5, vertscale = 0.3);
-pcr2 = PointCloud(vs2, ns2, 32);
+pcr = PointCloud(vs, ns, 32);
 # plane
-p_ae = (ϵ = 0.8, α=deg2rad(10));
+p_ae = (ϵ = 3, α=deg2rad(10));
 # cylidner
-cy_ae = (ϵ = 0.4, α=deg2rad(5));
+cy_ae = (ϵ = 3, α=deg2rad(10));
 # sphere
-sp_ae = (ϵ = 0.2, α=deg2rad(5));
-two_ae = AlphSilon(sp_ae, p_ae, cy_ae);
+sp_ae = (ϵ = 1, α=deg2rad(5));
+one_ae = AlphSilon(sp_ae, p_ae, cy_ae);
 # number of minimal subsets drawed in one iteration
 tt = 15;
 # probability that we found shapes
-ptt = 0.9
+ptt = 0.8
 # minimum shape size
 ττ = 900
 # maximum number of iteration
 itermax = 40000
-
-cand, extr = ransac(pcr2, two_ae, tt, ptt, ττ, itermax, 3, 500, true)
+# size of the minimal set
+draws = 3
+include("ransac.jl")
+cand, extr = ransac(pcr, one_ae, tt, ptt, ττ, itermax, draws, 500, true)
+#leftover = getrest(pcr);
 showtype(extr)
-showbytype(pcr2, extr)
-sc2 = showshapes(pcr2, extr)
-vbox(sc, sc2)
-sco = scatter(vs2)
-slo = scatter(pcr.vertices[getrest(pcr2)])
-m = hbox(slo, vbox(sco, sc))
-Makie.save("plot.png", m)
+sc = showshapes(pcr, extr)
+sc = showbytype(pcr, extr)
 
-
-estimatescore(1100, 16000, 1100)
+sc.center = false
+Makie.save("bytye.png", sc)
